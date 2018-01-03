5d3042f549ca75925b951590fb59d16d456737ff
RikiOktopan <rikioktopan1@gmail.com>
2018-01-03 16:48:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'bf707fa9-fef3-40f2-80dc-55d886c8de19'; //API SimSimi
+$key = '2984115d-a085-43a8-bc15-246fd1368880'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
